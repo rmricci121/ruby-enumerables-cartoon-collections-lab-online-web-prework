da68ebd7cfaeb95c0eb8d71c7737fd066436dd32
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 18:37:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheeses|
     cheeses == "chedder" || cheeses == "gouda" || cheeses == "camembert"
 end
