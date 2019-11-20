2357430c09b7518ab80d32e8074332328d079975
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 18:16:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,6 @@
 def roll_call_dwarves(argument)
   array = argument
   array.each_with_index {|item|,|index| puts #{index +1}. #{name}
-  
-  
 end
 
 def summon_captain_planet# code an argument here
