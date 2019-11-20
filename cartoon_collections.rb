7740aec1d04bdce6abfb74acf3ca8db99fb49906
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 18:31:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
-  # the array below is here to help
+  cheese.find 
   cheese_types = ["cheddar", "gouda", "camembert"]
   
 end
