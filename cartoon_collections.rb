c5c2367c6879ce24f1e533af2d3e004a2e7b3eeb
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 18:20:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   index = 0 
-  array.each_with_index {|namex, index| puts "#{index +1}. #{name}"}
+  dwarves.each_with_index {|namex, index| puts "#{index +1}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
